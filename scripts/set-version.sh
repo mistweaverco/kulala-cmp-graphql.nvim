768648bb7472c043126ae0eb80840c0df8af58c4
Marco Kellershoff <marco@kellershoff.net>
2024-08-12 23:27:37 +0200

    fix(ci): fix set-version script

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 update_lua_globals_version() {
   local tmp
   tmp=$(mktemp)
-  sed -e "s/VERSION = \".*\"/VERSION = \"$VERSION\"/" ./lua/kulala_cmp_graphql/globals/init.lua > "$tmp" && mv "$tmp" ./lua/kulala_cmp_graphql/globals/init.lua
+  sed -e "s/VERSION = \".*\"/VERSION = \"$VERSION\"/" ./lua/kulala-cmp-graphql/globals/init.lua > "$tmp" && mv "$tmp" ./lua/kulala-cmp-graphql/globals/init.lua
 }
 
 update_lua_globals_version
